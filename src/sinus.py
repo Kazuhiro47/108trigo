040ef5568671a0d4824e771cdbd033a0915b956f
Samuel Radat <samuel.radat@epitech.eu>
2017-03-13 11:32:28 +0100

    update

---
+++ src/sinus.py
@@ -0,0 +1,14 @@
+from matrix_fct.matrix import *
+
+def sin_calculus(m):
+	tmp = divide_matrix_by_one_number(factorielle(3), pow_matrix(m, 3))
+	res = substract_matrix(m, tmp)
+	n = 2
+	while (n <= 42):
+		tmp = divide_matrix_by_one_number(factorielle(2 * n + 1), pow_matrix(m, 2 * n + 1))
+		if (n % 2 == 0):
+			res = add_matrix(res, tmp)
+		else:
+			res = substract_matrix(res, tmp)
+		n += 1
+	return (res)
